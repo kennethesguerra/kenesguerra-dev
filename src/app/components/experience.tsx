4be07f4ea610a87a06f7435e6936ebdaedac71b3
Ken Esguerra <kenesguerra_dev@outlook.com>
2024-06-15 01:32:46 +0800

    revise grammars on experience page

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -18,8 +18,8 @@ export default function Experience() {
       companyIntro: "Asurion is a leading provider of device insurance, warranty & support services for cell phones, consumer electronics & home appliances",
       description: [
         "Launch new clients and new features for existing clients across the US and Latin America to the claim-filing platform called Horizon. Some of our big clients are AT&T and Verizon.",
-        "Actively helping on solving prod-related issues and processing stuck claims due to some error.",
-        "Prior to working on Horizon Platform for US & LatAm clients, I started as Site Reliability Engineer for Horizon APAC. We mainly do application support and develop a portal called Datazap Portal to automate data change required to solve a production issue."
+        "Actively helping on solving prod-related issues and processing stuck claims due to technical issues.",
+        "Prior to working with US and LatAm clients, I started as a Site Reliability Engineer for Horizon APAC. We specialized in application support and developed the Datazap Portal, designed to automate data changes required to efficiently resolve production issues."
       ],
       technologyUsed: ['React', 'GraphQL', 'Node.js', 'AWS Lambda', 'Oracle']
     },
@@ -28,8 +28,8 @@ export default function Experience() {
       jobTitle: "Customer Applications Engineer",
       companyIntro: "HCX offers innovative solutions and services that enable organizations’ HR and other Core units to leverage on leading edge platforms, systems and tools to improve productivity, enhance user experience and reduce costs.",
       description: [
-        "Lead the frontend development of two big projects such as ADA (Ayala Data Analytics) for Ayala Corporation and T3 (Time Tracking Tool) for HCX itself.",
-        "Enhanced and maintained the ARC (Ayala Rewards Circle) API for their ARC Mobile Application that handles rewards for Zing, Globe and BPI."
+        "Led the frontend development for two major projects: ADA (Ayala Data Analytics) for Ayala Corporation and T3 (Time Tracking Tool) for HCX.",
+        "Enhanced and maintained the ARC (Ayala Rewards Circle) API for the ARC Mobile Application, which manages rewards for Zing, Globe, and BPI."
       ],
       technologyUsed: ['React', 'Node.js', 'PHP', 'Laravel', 'Heroku', 'MySQL', 'PostgreSQL', 'Docker']
     },
@@ -39,8 +39,8 @@ export default function Experience() {
       companyIntro: "Deltek is the leading global provider of software and solutions for projectbased businesses.",
       description: [
         "Developed and maintained the Deltek Talent Management (formerly HRSmart) product using PHP, MySQL, jQuery and Smarty. DTM is a solution that helps an organization to handle human resourcing from start to finish.",
-        "Pioneered the development of CoreHR module of the DTM which manages employee information easily and efficiently. The module supports Effective Dating and Employee/Manager SelfService.",
-        "Maintained the Legacy versions of DTM for all the clients that still uses it until its sunset last 2018."
+        "As one of the pioneering team members, I contributed to the development of the CoreHR module within DTM, streamlining the management of employee information with features such as Effective Dating and Employee/Manager Self-Service functionality.",
+        "Maintained Legacy versions of DTM for clients until its sunset in 2018."
       ],
       technologyUsed: ['PHP', 'MySQL', 'AWS Aurora', 'Smarty', 'jQuery']
     },
@@ -49,9 +49,9 @@ export default function Experience() {
       jobTitle: "Developer", 
       companyIntro: "PhilWeb is the leading gaming technology provider in the Philippines. They are listed on the Philippine Stock Exchange (PSE:WEB)",
       description: [
-        "Built and maintained tools required to operate the casino such as its own POS System, Rewards Management, Membership, etc.",
-        "Developed APIs for Genesis Terminal Machines with collaboration with R&D Team in order the machine to communicate with the POS System.",
-        "Created a small framework to automate unit testing using Watir or Web Application Testing in Ruby."
+        "Developed and maintained essential tools for casino operations, including a proprietary POS system, Rewards Management, Membership platform, and more.",
+        "Developed APIs for Genesis Terminal Machines in collaboration with the R&D Team to facilitate communication between the machines and the POS System.",
+        "Developed a lightweight framework to automate unit testing utilizing Watir or Web Application Testing in Ruby."
       ],
       technologyUsed: ['PHP', 'Yii', 'MySQL', 'jQuery', 'Ruby']
     }
